da597a68f564efc9959ba24e418bdf88a931dc06
Nathan Cheung <nathancheung22@gmail.com>
2021-03-23 22:55:23 -0400

    added distribution path

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-community/async-storage";
+
+const useStickyState = (defaultValue, loadingDefaultValue, key) => {
+  const [value, setValue] = useState(loadingDefaultValue);
+
+  useEffect(() => {
+    const fetchFromAsyncStorage = async () => {
+      const stickyValue = await AsyncStorage.getItem(key);
+      const finalValue = stickyValue === null ? defaultValue : JSON.parse(stickyValue);
+      setValue(finalValue);
+    };
+    fetchFromAsyncStorage();
+  }, []);
+
+  useEffect(() => {
+    AsyncStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+module.exports = useStickyState;
